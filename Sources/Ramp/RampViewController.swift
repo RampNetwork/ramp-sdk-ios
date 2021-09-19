7869d0058493f0c630cd4ec52276ea63fb5eeb69
Mateusz Jabłoński <mateusz.piotr.jablonski@gmail.com>
2021-09-19 19:09:33 +0200

    Deleted unused events

--- Sources/Ramp/RampViewController.swift
+++ Sources/Ramp/RampViewController.swift
@@ -122,13 +122,10 @@ public final class RampViewController: UIViewController {
     
     private func handleIncomingEvent(_ event: IncomingEvent) {
         switch event {
-        case .close: closeRamp()
         case .kycInit(let payload): startPassbaseFlow(payload)
         case .purchaseCreated(let payload): delegate?.ramp(self, didCreatePurchase: payload.purchase)
         case .purchaseFailed: delegate?.rampPurchaseDidFail(self)
         case .widgetClose(let payload): handleCloseRampEvent(payload)
-        case .widgetConfigDone: break
-        case .openLink: break
         }
     }
     
