1025daf6709cbfcc8d23c570ea03ac31e8007311
Mateusz Jabłoński <mateusz.piotr.jablonski@gmail.com>
2023-02-21 18:46:30 +0100

    changed log tags

--- Sources/Ramp/Logger.swift
+++ Sources/Ramp/Logger.swift
@@ -3,13 +3,13 @@ import Foundation
 struct Logger {
     static func debug(_ message: String) {
         #if DEBUG
-        print("[RampSdk]", message)
+        print("[debug]", message)
         #endif
     }
     
     static func error(_ error: Error) {
         #if DEBUG
-        print("[RampSdk]", String(describing: error))
+        print("[error]", String(describing: error))
         #endif
     }
 }
